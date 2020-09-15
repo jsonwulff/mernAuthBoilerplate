343508c0fbb11f9c94f1110332c11c48e4560e51
Julian Wulff <julianwulff@gmail.com>
2020-09-15 16:26:28 +0200

    Added PORT

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
-const express = require("express");
-require("./db/connectDB");
-const authRoutes = require("./routes/auth"); // Import routes
+const express = require('express');
+require('./db/connectDB');
+const authRoutes = require('./routes/auth');
+const { PORT } = process.env;
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middelware for parsing incoming requests with JSON payloads
 app.use(express.json());
 
-app.use('/api', authRoutes)
+app.use('/api', authRoutes);
 
-app.listen(PORT, () =>
-  console.log(`Server is running on port ${PORT}...`)
-);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+
