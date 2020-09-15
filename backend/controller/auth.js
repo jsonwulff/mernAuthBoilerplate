e6ef9fa4d5a1dc24804a911cbd9303efab7e0b57
Julian Wulff <julianwulff@gmail.com>
2020-09-15 16:31:35 +0200

    Restructured db and models folder

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,7 +1,7 @@
-const User = require("../db/models/User");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const sgMail = require("@sendgrid/mail");
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const jwtSignupSecret = process.env.JWT_SIGNUP_SECRET;
@@ -15,17 +15,17 @@ exports.signup = (req, res) => {
       if (user) {
         return res
           .status(400)
-          .json({ Error: "User with this email already exists." });
+          .json({ Error: 'User with this email already exists.' });
       }
 
       const token = jwt.sign({ name, email, password }, jwtSignupSecret, {
-        expiresIn: "20m",
+        expiresIn: '20m',
       });
 
       const msg = {
-        to: "julianwulff@gmail.com", // ! This should be changed - only for testing purposes
+        to: 'julianwulff@gmail.com', // ! This should be changed - only for testing purposes
         from: `${process.env.SENDER_EMAIL}`, // ! This should be change - only for testing purposes
-        subject: "Account Activation Link",
+        subject: 'Account Activation Link',
         text: `and easy to do anywhere, even with Node.js`,
         html: `
           <h2>Please click on the given link to activate your account</h2>
@@ -38,7 +38,7 @@ exports.signup = (req, res) => {
           return res.json({ Error: err });
         }
         return res.json({
-          message: "Email has been sent, kindly activate your account",
+          message: 'Email has been sent, kindly activate your account',
         });
       });
     })
@@ -52,7 +52,7 @@ exports.activateAccount = (req, res) => {
   if (token) {
     jwt.verify(token, jwtSignupSecret, (err, decodedToken) => {
       if (err) {
-        return res.status(400).json({ Error: "Incorrect or expired link." });
+        return res.status(400).json({ Error: 'Incorrect or expired link.' });
       }
 
       const { name, email, password } = decodedToken;
@@ -61,7 +61,7 @@ exports.activateAccount = (req, res) => {
           if (user) {
             return res
               .status(400)
-              .json({ Error: "User with this email already exists." });
+              .json({ Error: 'User with this email already exists.' });
           }
 
           const newUser = new User({ name, email, password });
@@ -70,17 +70,17 @@ exports.activateAccount = (req, res) => {
           bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
               if (err) {
-                console.log("Error hashing the password: ", err);
+                console.log('Error hashing the password: ', err);
               }
 
               newUser.password = hash;
               newUser.save((err, user) => {
                 if (err) {
-                  console.log("Error in signup", err);
+                  console.log('Error in signup', err);
                   return res.status(400).json({ Error: err });
                 }
                 res.json({
-                  message: "Signup successfull",
+                  message: 'Signup successfull',
                   user,
                 });
               });
@@ -90,6 +90,6 @@ exports.activateAccount = (req, res) => {
         .catch((err) => res.json({ Error: err }));
     });
   } else {
-    return res.status(400).json({ Error: "Token was missing" });
+    return res.status(400).json({ Error: 'Token was missing' });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
-require('./db/connectDB');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
-const { PORT } = process.env;
+const { PORT, MONGO_USER, MONGO_PASS } = process.env;
 
 const app = express();
+
+// Conenct to DB
+const db = `mongodb://${MONGO_USER}:${MONGO_PASS}@mongo:27017/exercise?authSource=admin`;
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('MongoDB succesfully connected'))
+  .catch((err) => console.log('DB connection error: ', err));
 
 // Middelware for parsing incoming requests with JSON payloads
 app.use(express.json());
@@ -11,4 +22,3 @@ app.use(express.json());
 app.use('/api', authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
-
