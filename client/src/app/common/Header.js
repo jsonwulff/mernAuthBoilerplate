403fb89155d44d3f373e7de4d6bc504f4adcc1cc
Julian Wulff <julianwulff@gmail.com>
2020-09-19 23:15:43 +0200

    Removed unused import

--- client/src/app/common/Header.js
+++ client/src/app/common/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import { Toolbar, Typography, Button } from '@material-ui/core';
+import { Toolbar, Button } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
